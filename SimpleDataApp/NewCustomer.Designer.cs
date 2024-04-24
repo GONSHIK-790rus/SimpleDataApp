0f9df3137d1f3daabd70a2575f5c19e30149182c
Egor Pilyugin <pilyugin06@bk.ru>
2024-04-24 13:30:04 +0300

    26 Практическая работа по Гаиашвили

---
+++ SimpleDataApp/NewCustomer.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace SimpleDataApp
+{
+    partial class NewCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numOrderAmount = new System.Windows.Forms.NumericUpDown();
+            this.dtpOrderDate = new System.Windows.Forms.DateTimePicker();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.btnAddAnotherAccount = new System.Windows.Forms.Button();
+            this.btnAddFinish = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавление учётной записи";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя заказчика:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(173, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Идентификатор клиента:";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(152, 73);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(169, 22);
+            this.txtCustomerName.TabIndex = 3;
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Location = new System.Drawing.Point(216, 107);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.ReadOnly = true;
+            this.txtCustomerID.Size = new System.Drawing.Size(105, 22);
+            this.txtCustomerID.TabIndex = 4;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(346, 107);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(174, 23);
+            this.btnCreateAccount.TabIndex = 5;
+            this.btnCreateAccount.Text = "Создать учётную запись";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Создание заказа";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Сумма заказа:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Дата заказа:";
+            // 
+            // numOrderAmount
+            // 
+            this.numOrderAmount.Location = new System.Drawing.Point(146, 204);
+            this.numOrderAmount.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numOrderAmount.Name = "numOrderAmount";
+            this.numOrderAmount.Size = new System.Drawing.Size(120, 22);
+            this.numOrderAmount.TabIndex = 9;
+            // 
+            // dtpOrderDate
+            // 
+            this.dtpOrderDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpOrderDate.Location = new System.Drawing.Point(146, 241);
+            this.dtpOrderDate.Name = "dtpOrderDate";
+            this.dtpOrderDate.Size = new System.Drawing.Size(120, 22);
+            this.dtpOrderDate.TabIndex = 10;
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(346, 243);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(174, 23);
+            this.btnPlaceOrder.TabIndex = 11;
+            this.btnPlaceOrder.Text = "Разместить заказ";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
+            // 
+            // btnAddAnotherAccount
+            // 
+            this.btnAddAnotherAccount.Location = new System.Drawing.Point(294, 318);
+            this.btnAddAnotherAccount.Name = "btnAddAnotherAccount";
+            this.btnAddAnotherAccount.Size = new System.Drawing.Size(226, 23);
+            this.btnAddAnotherAccount.TabIndex = 12;
+            this.btnAddAnotherAccount.Text = "Добавить другую учётную запись";
+            this.btnAddAnotherAccount.UseVisualStyleBackColor = true;
+            this.btnAddAnotherAccount.Click += new System.EventHandler(this.btnAddAnotherAccount_Click);
+            // 
+            // btnAddFinish
+            // 
+            this.btnAddFinish.Location = new System.Drawing.Point(67, 318);
+            this.btnAddFinish.Name = "btnAddFinish";
+            this.btnAddFinish.Size = new System.Drawing.Size(134, 23);
+            this.btnAddFinish.TabIndex = 13;
+            this.btnAddFinish.Text = "Готово";
+            this.btnAddFinish.UseVisualStyleBackColor = true;
+            this.btnAddFinish.Click += new System.EventHandler(this.btnAddFinish_Click);
+            // 
+            // NewCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 392);
+            this.Controls.Add(this.btnAddFinish);
+            this.Controls.Add(this.btnAddAnotherAccount);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.dtpOrderDate);
+            this.Controls.Add(this.numOrderAmount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "NewCustomer";
+            this.Text = "Новая учётная запись и заказ";
+            this.Load += new System.EventHandler(this.NewCustomer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numOrderAmount;
+        private System.Windows.Forms.DateTimePicker dtpOrderDate;
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.Button btnAddAnotherAccount;
+        private System.Windows.Forms.Button btnAddFinish;
+    }
+}
